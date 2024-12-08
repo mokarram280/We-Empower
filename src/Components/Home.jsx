5ee957b7cb0436ba41ac17d088471702e6fa6f0d
mokarram280 <mdmokarramhossain98@gmail.com>
2024-12-08 11:12:21 +0600

    Home section added

---
+++ src/Components/Home.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import img from '/src/assets/edwin-andrade-4V1dC_eoCwg-unsplash 1.png'
+
+function Home() {
+  return (
+    <section style={{backgroundImage:`url(${img})`}} className='bg-no-repeat bg-cover'>
+        <div className='text-center py-20 px-12 space-y-8 w-full'>
+            <h5 className='text-md md:text-lg lg:text-xl font-bold text-white'>Discover <span className='text-[#F44335]'>-</span> Interact <span className='text-[#F44335]'>-</span> Engage </h5>
+            <h1 className='text-2xl md:text-4xl lg:text-5xl font-bold text-white max-w-2xl mx-auto'>Connecting <span className='text-[#F44335] underline font-semibold'>talent</span> to opportunity through <span className='text-[#F44335] underline font-semibold'>skills</span></h1>
+            <p className='text-sm md:text-md lg:text-xl text-gray-400 max-w-3xl mx-auto'>We forster a skill-focussed environment enabling students to navigate their career with their self-paced learning journey and businesses to gain market-ready talent.</p>
+            <button className='bg-white px-5 py-2 font-semibold rounded-full text-[#F44335] text-sm'>Ask questions</button>
+        </div>
+        <div>
+          <img src="/Wave.svg" alt="" />
+        </div>
+    </section>
+  )
+}
+
+export default Home;
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import {AppBar, Button, Drawer, IconButton, List, ListItemButton, ListItemIcon, ListItemText, Tab, Tabs, Toolbar, Typography, useMediaQuery, useTheme} from '@mui/material'
+import { IoMenuSharp } from "react-icons/io5";
+
+function Navbar() {
+    const [value,setValue] = useState(0);
+    const [drawerOpen,setDrawerOpen] = useState(false);
+    const theme = useTheme();
+    const isMatch = useMediaQuery(theme.breakpoints.down('md'));
+    const Pages = ['Home','About us','Upskill','Cotact us'];
+    const PAGE = ['Home','About us','Upskill','Cotact us','Register Interest'];
+  return (
+    <>
+        <AppBar position='static' elevation={0} sx={{backgroundColor:'white',maxWidth:'1280px',mx:'auto'}}>
+            <Toolbar sx={{color:'black'}}>
+                <Typography>
+                    <img className='w-32' src="/c4u full logo_2022 1.svg" alt="" />
+                </Typography>
+                {
+                    isMatch ? (
+                        <>
+                          <Drawer open={drawerOpen} onClose={()=> setDrawerOpen(false)}>
+                            <List>
+                                {
+                                    PAGE.map((page,index)=>(
+                                        <ListItemButton key={index} onClick={()=> setDrawerOpen(false)}>
+                                            <ListItemIcon>
+                                                <ListItemText className='capitalize'>{page}</ListItemText>
+                                            </ListItemIcon>
+                                        </ListItemButton>
+                                    ))
+                                }
+                            </List>
+                          </Drawer>
+                          <IconButton sx={{color:'black',marginLeft:'auto'}} onClick={()=> setDrawerOpen(!drawerOpen)}>
+                              <IoMenuSharp />
+                          </IconButton>
+                        </>
+                    ):(
+                        <>
+                          <Tabs sx={{marginLeft:'auto'}} 
+                          textColor='inherit'
+                          indicatorColor='inherit'
+                          value={value}
+                          onChange={(e,value)=> setValue(value)}
+                          >
+                            {
+                                Pages.map((page,index)=>(
+                                    <Tab key={index} label={page}></Tab>
+                                ))
+                            }
+                          </Tabs>
+                          <Button sx={{marginLeft:'auto',color:'white',backgroundColor:'#F44335'}}>
+                            Register Interest
+                          </Button>
+                        </>
+                    )
+                }
+            </Toolbar>
+        </AppBar>
+    </>
+  )
+}
+
+export default Navbar;
